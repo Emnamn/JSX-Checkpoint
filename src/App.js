f0c1ed5e5f960d983f22d9bb89240b0559688a94
Emnamn <emna.manai.7@gmail.com>
2020-10-11 10:47:09 +0100

    Add all my files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import imageInSrc from "./imageInSrc.png"
+import './Style.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<div style={{ border:"solid,1px,black",maxwidth:"100vw"}}>
+ <h1 className={"red", "title"}> Emna Manai </h1>
+ {'\n'} 
+ <img src={imageInSrc} alt="srcimg" />
+ {'\n'} 
+ <img src="/imageInPublic.jpg" alt="pubimg" />
+
+</div>
   );
 }
-
 export default App;
